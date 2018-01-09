00488ceff3bb5e6eb01c1a2a6c30143521cd6b60
amir.gabay <amir.gabay@codefresh.io>
2018-01-09 20:40:41 +0000

    Updated launchpip.sh

--- launchpip.sh
+++ launchpip.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-git push -f https://github.com/amir-codefresh/demochat master:master
+git remote add origin git@github.com:amir-codefresh/demochat.git
+git push -f origin master
 curl 'https://g.codefresh.io/api/builds/5a54e5fc3d7f460001af3464' -H 'content-type:application/json; charset=utf-8'  -H 'x-access-token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1YTBjNTI4OTU4NjFjYTAwMDFjYTRjNjEiLCJhY2NvdW50SWQiOiI1YTNhMmE0NDU1MTg2MTAwMDE2MzdjODciLCJpYXQiOjE1MTU0ODUxOTYsImV4cCI6MTUxODA3NzE5Nn0.R2lrYe-qPjjtgO4z3xe2N6A0z51-P5eT-2kdxTG_zzU' --data-binary '{"repoOwner":"codefresh-demo","repoName":"demochat","serviceId":"5a54e5fc3d7f460001af3464","branch":"master","type":"build"}' --compressed
